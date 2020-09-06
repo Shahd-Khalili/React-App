5ebd9e84bb9b787791ef1bbc46340c19a4aaf8cb
Shahd Khalili <shahdkhalili59@gmail.com>
2020-09-06 17:12:01 +0300

    use react router to show two pages the home page is main and about is the black interface how contain the author information

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 //Parent Component
 import React from 'react';
-import AuthorInformation from './containers/AuthorInformation'
-import './App.scss';
+import { BrowserRouter , Route } from 'react-router-dom'
+import Nav from './components/Nav/Nav'
 import FOlioPage from './containers/FOlioPage';
+import AuthorInformation from './containers/AuthorInformation';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-     <AuthorInformation/>
-     <FOlioPage/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <Route  exact path="/" component={FOlioPage}/>
+        <Route path="/About" component={AuthorInformation}/>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './NavStyle.scss';
+
+const Nav = () =>{
+    return (
+        <div className="Navbar">
+            <div className="Navbar__Continer">
+                <a href="#F" className="Continer__Logo">FOlio</a>
+                <ul>
+                   <a href="/">Home</a>
+                   <a href="/About">About</a>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
